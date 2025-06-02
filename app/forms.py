ee7fe910c21b621b2dfe64b190abaa7109928130
Solomiia Vikovych <solomiiavikovych@icloud.com>
2025-06-02 18:05:42 +0200

    2025-06-02

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from wtforms import Form, StringField, SubmitField, validators
 
-class ProdyctIdForm(Form):
+class ProductIdForm(Form):
     product_id = StringField("Product id", name='product_id', validators=[
         validators.DataRequired(message='Product id is required'),
         validators.Length(min=6, max=10, message='Product id should have between 6 and 10 characters(inclusive)'),
